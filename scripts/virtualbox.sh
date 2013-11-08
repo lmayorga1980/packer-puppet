97ada6236e4d4341cb1bff03222b6cc03c55d42a
Luis Mayorga <luis.mayorga@nreca.coop>
2013-11-08 10:00:07 -0500

    fix oracle kernel headers

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,10 +1,13 @@
 # Bail if we are not running inside VirtualBox.
-[ -f /etc/init.d/vboxadd ] || {
-  exit 0
-}
-
-mkdir -p /mnt/virtualbox
-mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
-sh /mnt/virtualbox/VBoxLinuxAdditions.run
-umount /mnt/virtualbox
-rm -rf /home/vagrant/VBoxGuest*.iso
+if [ -f /home/vagrant/VBoxGuestAdditions_4.3.2.iso ];
+then
+  echo "installing virtual box guest additions"
+  mkdir -p /mnt/virtualbox
+  mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+  sh /mnt/virtualbox/VBoxLinuxAdditions.run
+  umount /mnt/virtualbox
+ #rm -rf /home/vagrant/VBoxGuest*.iso
+else
+  echo "this is not a oracle virtual box vm"
+  exit 0  
+fi

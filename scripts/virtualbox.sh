984b27507bf55db701823f51d55b1815fa7eb49d
Luis Mayorga <luis.mayorga@nreca.coop>
2013-11-08 10:35:17 -0500

    remove extra packages from centos,oracle. remove guestadditions.iso after installation on virtualbox.sh

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -6,7 +6,7 @@ then
   mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
   sh /mnt/virtualbox/VBoxLinuxAdditions.run
   umount /mnt/virtualbox
- #rm -rf /home/vagrant/VBoxGuest*.iso
+  rm -rf /home/vagrant/VBoxGuest*.iso
 else
   echo "this is not a oracle virtual box vm"
   exit 0  
